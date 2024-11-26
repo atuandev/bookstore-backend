7742fd274d36346ba875ff919f129f1ab7812dc7
atuandev <atuandev@gmail.com>
2024-11-26 08:56:30 +0700

    refactor: update user

--- src/main/java/com/iuh/dto/request/UserUpdateRequest.java
+++ src/main/java/com/iuh/dto/request/UserUpdateRequest.java
@@ -1,15 +1,11 @@
 package com.iuh.dto.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
-
-import java.util.List;
 
 @Data
 @Builder
@@ -18,21 +14,8 @@ import java.util.List;
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE)
 public class UserUpdateRequest {
 
-    @Size(min = 4, message = "INVALID_USERNAME")
-    String username;
-
-    @Size(min = 6, message = "INVALID_PASSWORD")
-    String password;
-
     @NotBlank(message = "INVALID_NAME")
     String name;
 
-    @Email(message = "INVALID_EMAIL")
-    String email;
-
     String avatar;
-
-    Boolean status;
-
-    List<String> roles;
 }
