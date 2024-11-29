de58c221ec5435e44693c3f048c3c3381cd916aa
HieuTrungMc <phantrunghieu1109@gmail.com>
2024-11-30 03:59:10 +0700

    services update

--- src/main/java/com/iuh/service/impl/StatisticsServiceImpl.java
+++ src/main/java/com/iuh/service/impl/StatisticsServiceImpl.java
@@ -1,9 +1,13 @@
 package com.iuh.service.impl;
 
+import java.time.Year;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.iuh.dto.response.BookStatisticsResponse;
+import com.iuh.entity.BookStatistics;
 import com.iuh.entity.Statistics;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import com.iuh.dto.response.StatisticsResponse;
@@ -24,6 +28,7 @@ public class StatisticsServiceImpl implements StatisticsService {
     StatisticsRepository statisticsRepository;
 
     @Override
+    @PreAuthorize("hasRole('ADMIN')")
     public List<StatisticsResponse> getStatisticsByYear(int year) {
         List<Statistics> listStat = statisticsRepository.getMonthlyReportByYear(year);
         return listStat.stream()
@@ -33,6 +38,44 @@ public class StatisticsServiceImpl implements StatisticsService {
                         stat.getTotalImportCost(),
                         stat.getTotalSoldAmount(),
                         stat.getTotalProfit()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<StatisticsResponse> getAllStatistics() {
+        List<Statistics> listStat = statisticsRepository.getAllTimeStats();
+        return listStat.stream()
+                .map(stat -> new StatisticsResponse(
+                        stat.getMonth(),
+                        stat.getTotalOrders(),
+                        stat.getTotalImportCost(),
+                        stat.getTotalSoldAmount(),
+                        stat.getTotalProfit()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<BookStatisticsResponse> getMostSellingBooks(int limit) {
+        List<BookStatistics> listStat = statisticsRepository.getMostSellingBooks();
+        return listStat.subList(0, limit).stream()
+                .map(stat -> new BookStatisticsResponse(
+                        stat.getBookId(),
+                        stat.getBookTitle(),
+                        stat.getTotalSold()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<BookStatisticsResponse> getMostSellingBooksByMonth(int month) {
+        int year = Year.now().getValue();
+        List<BookStatistics> listStat = statisticsRepository.getMostSellingBooksByMonth(month, year);
+        return listStat.stream()
+                .map(stat -> new BookStatisticsResponse(
+                        stat.getBookId(),
+                        stat.getBookTitle(),
+                        stat.getTotalSold()))
                 .collect(Collectors.toList());
     }
 
