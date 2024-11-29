3ec3145fd4185dd67537d2536a9f0945658961d7
HieuTrungMc <phantrunghieu1109@gmail.com>
2024-11-30 03:58:23 +0700

    controller: add all-time stats, most books sold (with months support)

--- src/main/java/com/iuh/controller/StatisticsController.java
+++ src/main/java/com/iuh/controller/StatisticsController.java
@@ -1,9 +1,8 @@
 package com.iuh.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.iuh.dto.response.BookStatisticsResponse;
+import jakarta.validation.constraints.Min;
+import org.springframework.web.bind.annotation.*;
 
 import com.iuh.dto.ApiResponse;
 import com.iuh.dto.response.StatisticsResponse;
@@ -14,6 +13,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
+import java.util.Collections;
 import java.util.List;
 
 @Tag(name = "Statistics Controller")
@@ -22,8 +22,14 @@ import java.util.List;
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
 public class StatisticsController {
-
     StatisticsService statisticsService;
+
+    @Operation(summary = "Get all-time statistics")
+    @GetMapping("all")
+    public ApiResponse<List<StatisticsResponse>> getAllStatistics() {
+        var statistics = statisticsService.getAllStatistics();
+        return ApiResponse.<List<StatisticsResponse>>builder().data(statistics).build();
+    }
 
     @Operation(summary = "Get statistics by year")
     @GetMapping("/year/{year}")
@@ -31,5 +37,23 @@ public class StatisticsController {
         var statistics = statisticsService.getStatisticsByYear(year);
         return ApiResponse.<List<StatisticsResponse>>builder().data(statistics).build();
     }
+
+    @Operation(summary = "Get most selling books")
+    @GetMapping("/books/all")
+    public ApiResponse<List<BookStatisticsResponse>> getMostSellingBooks(
+            @Min(1) @RequestParam(defaultValue = "10", required = false) int limit
+    ) {
+        var statistics = statisticsService.getMostSellingBooks(limit);
+        return ApiResponse.<List<BookStatisticsResponse>>builder().data(statistics).build();
+    }
+    @Operation(summary = "Get most selling books by month (in current year)")
+    @GetMapping("/books/month/{month}")
+    public ApiResponse<List<Object>> getMostSellingBooksByMonth(
+            @PathVariable int month
+    ) {
+        var statistics = statisticsService.getMostSellingBooksByMonth(month);
+        return ApiResponse.<List<Object>>builder().data(Collections.singletonList(statistics)).build();
+    }
+
 
 }
